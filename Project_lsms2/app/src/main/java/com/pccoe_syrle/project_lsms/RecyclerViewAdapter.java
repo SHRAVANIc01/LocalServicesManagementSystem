48663606ac83bd1c70287f6f070940dac0c0f1b4
Aryan Sagavekar <sagavekararyan@gmail.com>
2023-05-20 20:23:37 +0530

    added fonts to our project
    Signed-off-by: Aryan Sagavekar <sagavekararyan@gmail.com>

---
+++ Project_lsms2/app/src/main/java/com/pccoe_syrle/project_lsms/RecyclerViewAdapter.java
@@ -0,0 +1,50 @@
+package com.pccoe_syrle.project_lsms;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.MyViewHolder> {
+    List<ModelClass> mList;
+
+    public RecyclerViewAdapter(List<ModelClass> mList) {
+        this.mList = mList;
+    }
+
+    @NonNull
+    @Override
+    public RecyclerViewAdapter.MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.service_display_layout,parent,false);
+        RecyclerViewAdapter.MyViewHolder vh = new RecyclerViewAdapter.MyViewHolder(v);
+        return vh;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerViewAdapter.MyViewHolder holder, int position) {
+        holder.names.setText(mList.get(position).getName());
+        holder.ids.setText(mList.get(position).getId());
+    }
+
+    @Override
+    public int getItemCount() {
+        return mList.size();
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder{
+        TextView names, ids;
+        ImageView imgs;
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+            names = itemView.findViewById(R.id.Names);
+            ids = itemView.findViewById(R.id.ids);
+            imgs = itemView.findViewById(R.id.imgs);
+        }
+    }
+}
--- Project_lsms2/app/src/main/java/com/pccoe_syrle/project_lsms/ui/home/HomeFragment.java
+++ Project_lsms2/app/src/main/java/com/pccoe_syrle/project_lsms/ui/home/HomeFragment.java
@@ -4,16 +4,25 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.pccoe_syrle.project_lsms.ModelClass;
+import com.pccoe_syrle.project_lsms.R;
+import com.pccoe_syrle.project_lsms.RecyclerViewAdapter;
 import com.pccoe_syrle.project_lsms.databinding.FragmentHomeBinding;
 
-public class HomeFragment extends Fragment {
+import java.util.ArrayList;
 
+public class HomeFragment extends Fragment {
+    EditText mSearch;
+    RecyclerView displayServices;
     private FragmentHomeBinding binding;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
@@ -24,6 +33,20 @@ public class HomeFragment extends Fragment {
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
+        mSearch = binding.searchBar;
+        displayServices = binding.ServicesView;
+        displayServices.setHasFixedSize(true);
+        ArrayList<ModelClass> listitem = new ArrayList<ModelClass>();
+
+        listitem.add(new ModelClass("Hello","3", R.drawable.baseline_person_outline_24));
+        listitem.add(new ModelClass("Bello","2",R.drawable.baseline_person_outline_24));
+        listitem.add(new ModelClass("Mello","1",R.drawable.baseline_person_outline_24));
+        listitem.add(new ModelClass("Khelo","0",R.drawable.baseline_person_outline_24));
+
+        LinearLayoutManager llm = new LinearLayoutManager(root.getContext(),LinearLayoutManager.HORIZONTAL,false);
+        displayServices.setLayoutManager(llm);
+        RecyclerViewAdapter adapter = new RecyclerViewAdapter(listitem);
+        displayServices.setAdapter(adapter);
         return root;
     }
 
