ff131b4c664411b8ca990cf5122508459b6302fe
Aryan Sagavekar <sagavekararyan@gmail.com>
2023-05-22 20:19:13 +0530

    database connectivity completed for login and signup
    Signed-off-by: Aryan Sagavekar <sagavekararyan@gmail.com>

--- Project_lsms2/app/src/main/java/com/pccoe_syrle/project_lsms/loginActivity.java
+++ Project_lsms2/app/src/main/java/com/pccoe_syrle/project_lsms/loginActivity.java
@@ -2,12 +2,16 @@ package com.pccoe_syrle.project_lsms;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.vishnusivadas.advanced_httpurlconnection.PutData;
 
 public class loginActivity extends AppCompatActivity {
 
@@ -33,9 +37,40 @@ public class loginActivity extends AppCompatActivity {
         });
 
         login.setOnClickListener(v -> {
-            Intent intent = new Intent(loginActivity.this, MainActivity.class);
-            intent.putExtra(key,"Homepage");
-            loginActivity.this.startActivity(intent);
+            String susername,spassword;
+            susername = username.getText().toString();
+            spassword = password.getText().toString();
+            if(!susername.equals("") && !spassword.equals("")) {
+                Handler handler = new Handler();
+                handler.post(() -> {
+                    String[] field = new String[2];
+                    field[0] = "username";
+                    field[1] = "password";
+
+                    String[] data = new String[2];
+                    data[0] = susername;
+                    data[1] = spassword;
+
+                    PutData pd = new PutData("http://192.168.1.5/Loginsignin/login.php","POST",field,data);
+                    if (pd.startPut()){
+                        if(pd.onComplete()){
+                            String result = pd.getResult();
+                            if(result.equals("Login Success")){
+                                Toast.makeText(loginActivity.this, result, Toast.LENGTH_SHORT).show();
+                                Intent intent = new Intent(loginActivity.this, MainActivity.class);
+                                startActivity(intent);
+                                finish();
+                            }
+                            else {
+                                Toast.makeText(loginActivity.this, result, Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    }
+                });
+            }
+            else {
+                Toast.makeText(loginActivity.this, "Username or password error", Toast.LENGTH_SHORT).show();
+            }
         });
     }
 }
